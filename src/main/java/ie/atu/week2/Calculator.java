575176b533bc7bdda823101d096456cfa2ed4a23
Alvin Olayemi <g00428825@atu.ie>
2025-10-04 14:44:29 +0100

    Added operator switch to calculator class

--- src/main/java/ie/atu/week2/Calculator.java
+++ src/main/java/ie/atu/week2/Calculator.java
@@ -1,8 +1,12 @@
 package ie.atu.week2;
 
+import static org.apache.el.lang.ELArithmetic.*;
+
 public class Calculator
 {
 private int x,y,z;
+public String op;
+
 
 public Calculator(int x , int y, int z)
 {
@@ -11,8 +15,29 @@ public Calculator(int x , int y, int z)
     this.y = y;
     this.z = z;
 
+    switch(op)
+    {
+        case "add":
+            add(x,y);
+            break;
+
+        case "Subtract":
+            subtract(x,y);
+            break;
+
+        case "Multiply":
+            multiply(x,y);
+            break;
+
+        case "Divide":
+            divide(x,y);
+            break;
+    }
+
 
 }
+
+
 
     public int getX()
     {
@@ -29,3 +54,6 @@ public Calculator(int x , int y, int z)
         return z;
     }
 }
+
+
+
